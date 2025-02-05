26814c303651dd7e1d6cd91e1150852b23722b24
Shiv Arora <shiv.arora@deeplogictech.com>
2025-02-05 18:33:04 +0530

    Added extractData function

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const https = require("https");
-const url="www.time.com";
+const url="https://time.com/";
 
-function fetchHTML(url){
+function fetchHTML(url,callback){
     https.get(url,(res)=> {
         let data= "";
 
         res.on("data",(chunk)=> {
-            chunk+=data;
+            data+=chunk;
         });
 
-        res.on(end, ()=> {
+        res.on("end", ()=> {
             callback(null,extractData(data));
         });
     }).on("error",(err)=>{
@@ -17,3 +17,30 @@ function fetchHTML(url){
     });
 }
 
+function extractData(data){
+    const articles =[];
+
+    const articleMatches =[...data.matchAll(/<div class="latest-stories__item.*?href="(\/\d{4}\/\d{2}\/\d{2}\/.*?)".*?>(.*?)<\/div>/g)];
+
+    for(let match of articleMatches){
+        if(articles.length >=5){
+            break;
+        }
+
+        const articleUrl = url+match[1];
+        const title = match[2].replace(/<.*?>/g, "").trim();
+
+        if (title) {
+            articles.push({ title, url: articleUrl });
+        }
+    }
+    return JSON.stringify({ latest_news: articles }, null, 2);
+}
+
+fetchHTML(url, (err, jsonData) => {
+    if (err) {
+        console.error("Error:", err);
+    } else {
+        console.log(jsonData);
+    }
+});
